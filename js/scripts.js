948ee1a11c0bea6e6463c19cd38b3385488869e7
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2020-12-29 22:50:50 -0500

    Added Pokemon to script.js
    - added array of pokemon with properties to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert('hello world');
+//creates array of pokemon with properties name, type, weight, and gender
 
-let favoriteFood = 'filet';
-document.write(favoriteFood);
+
+let pokemonList = [
+    {
+        name:'vulpix',
+        type:['fire'],
+        weight: 55,
+        gender: 'female',
+    }
+    {
+        name:'bulbasoar',
+        type:['grass'],
+        weight:70,
+        gender:'male',
+    }
+    {
+        name: 'torracat',
+        type:['fire'],
+        weight:120,
+        gender: 'female',
+    }
+]
