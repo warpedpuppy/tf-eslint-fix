490a3d8c3f367d784bb83919c27cc53310c8459b
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2020-12-31 20:06:58 -0500

    Added for loop & stylized
    - added for loop to print list of pokemon and their weight
    - stylized page with css giving background & text color & choosing font-style
    - added title & centered text

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,33 @@
 
 let pokemonList = [
     {
-        name:'vulpix',
+        name:'Vulpix',
         type:['fire'],
         weight: 55,
         gender: 'female',
     },
     {
-        name:'bulbasoar',
+        name:'Bulbasoar',
         type:['grass'],
         weight:70,
         gender:'male',
     },
     {
-        name: 'torracat',
+        name: 'Torracat',
         type:['fire'],
         weight:120,
         gender: 'female',
     }
 ]
+
+
+//Prints list of pokemon & their weight
+for(i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + ' (Weight: ' +  pokemonList[i].weight + ')');
+        if (pokemonList[i].weight >= 100){
+            document.write(' Wow! Thats big!' + '<br>');
+        }
+        else{
+            document.write("<br>");
+        }
+}
