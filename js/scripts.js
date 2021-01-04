96bcde6e584555a6c801ef476e61037defc4db11
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2021-01-04 11:56:05 -0500

    Update script.js (IIFE)
    Wrap in IIFE & update for.each loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,55 @@
 //creates array of pokemon with properties name, type, weight, and gender
-
-
-let pokemonList = [
-    {
-        name:'Vulpix',
-        type:['fire'],
-        weight: 55,
-        gender: 'female',
-    },
-    {
-        name:'Bulbasoar',
-        type:['grass'],
-        weight:70,
-        gender:'male',
-    },
-    {
-        name: 'Torracat',
-        type:['fire'],
-        weight:120,
-        gender: 'female',
-    }
-]
-
-
-function myLoop(pokemon) {
-    document.write(pokemon.name + '(Weight: ' + pokemon.weight + ')');
-    if(pokemon.weight >= 100){
-        document.write(" - Wow! Thats big!");
-    }
-    else {
-        document.write('<br>');
-    }
-}
-
-
-
-
-pokemonList.forEach(myLoop);
-
-//Prints list of pokemon & their weight
-/*for(i = 0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + ' (Weight: ' +  pokemonList[i].weight + ')');
-        if (pokemonList[i].weight >= 100){
-            document.write(' Wow! Thats big!' + '<br>');
+//wrapped in IIFE
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name:'Vulpix',
+            type:['fire'],
+            weight: 55,
+        },
+        {
+            name:'Bulbasoar',
+            type:['grass'],
+            weight:70,
+        },
+        {
+            name: 'Torracat',
+            type:['fire'],
+            weight:120,
         }
+    ];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        if(typeof pokemon !== 'object'
+         && "name" in pokemon
+         && "type" in pokemon
+         && "weight" in pokemon
+
+     ){
+        return document.write('Not an object');
+      }
         else{
-            document.write("<br>");
-}
-}*/
+            pokemonList.push(pokemon);
+            }
+    }
+    return {
+        getAll: getAll,
+        add: add,
+    };
+})();
+
+//adds a pokemon to repository
+pokemonRepository.add({
+    name: 'Pikachu',
+    type: ['electric'],
+    weight:30,
+});
+
+//writes pokemon list
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + " (type: " + pokemon.type + ")" + " (weight " + pokemon.weight + ")" + "<br>");
+});
